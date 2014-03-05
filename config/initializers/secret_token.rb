82dac766769367b28a957825f0d1f15303c69806
Van Der Putte Guillaume <vdp.guillaume@gmail.com>
2014-03-05 10:31:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManipalProject::Application.config.secret_key_base = '8b88b73aa392879fb0f7cb173422124a7b4b463647303223934c109f61d9f8844005cdee4c4e9221b61e9e30764e5afbe71815b0aaf859af8312a54afc20182c'
