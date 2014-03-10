a94c33ee2498f853fcf5a09b09fd2b7df2b1bff1
Van Der Putte Guillaume <vdp.guillaume@gmail.com>
2014-03-10 11:04:52 +0530

    inscription done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,24 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
     @title = "Inscription"
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Bienvenue dans l'Application Exemple!"
+      redirect_to @user
+    else
+      @title = "Inscription"
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nom, :email, :password, :salt, :encrypted_password)
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,58 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "POST 'create'" do
+
+    describe "echec" do
+
+      before(:each) do
+        @attr = { :nom => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "ne devrait pas creer d'utilisateur" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "devrait avoir le bon titre" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Inscription")
+      end
+
+      it "devrait rendre la page 'new'" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :nom => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "devrait creer un utilisateur" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "devrait rediriger vers la page d'affichage de l'utilisateur" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "devrait avoir un message de bienvenue" do
+        post :create, :user => @attr
+        flash[:success].should =~ /Bienvenue dans l'Application Exemple/i
+      end
+    end
+  end
+
+
   describe "GET 'show'" do
 
     before(:each) do
@@ -34,6 +86,7 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
+
 
   describe "GET 'new'" do
     it "devrait reussir" do
